e9231e23f28d135bf2f4fde36a8c5a83139b2b34
Grzegorz Bernat <g.bernat@greggy.pl>
2017-04-13 20:26:05 +0000

    Initial commit with working reading and formatting Mifare Clasic

---
+++ examples/server.js
@@ -0,0 +1,56 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var ndef = require('ndef');
+var mifaretools = require('..');
+
+server.listen(8080);
+console.log("Listening for new clients on port 8080");
+
+app.get('/', function(request, response) {
+    response.send('Reading constantly');
+    setInterval(function() {
+        mifaretools.read(function(err, uid, data) {
+            if (uid != undefined && uid != null) {
+                console.log("UID: " + uid);
+            }
+            if (err != undefined && err != null) {
+                console.error(new Error(err.replace(/\n$/, "")));
+            }
+            if (data != undefined && data != null) {
+                var message = ndef.decodeMessage(data.toJSON());
+                console.log("Found NDEF message with " + message.length +
+                    (message.length === 1 ? " record" : " records"));
+                console.log(ndef.stringify(message));
+            }
+        });
+    }, 1000);
+});
+
+app.get('/format', function(request, response) {
+    response.send('Formatting...');
+    mifaretools.format(function(err) {
+        if (err) {
+          console.error(new Error(err.replace(/\n$/, "")));
+        } else {
+            console.log("Format completed");
+        }
+    });
+});
+
+app.get('/read', function(request, response) {
+    response.send('Reading...');
+    mifaretools.read(function(err, uid, data) {
+        if (uid != undefined && uid != null) {
+            console.log("UID: " + uid);
+        }
+        if (err != undefined && err != null) {
+            console.error(new Error(err.replace(/\n$/, "")));
+        }
+        if (data != undefined && data != null) {
+            var message = ndef.decodeMessage(data.toJSON());
+            console.log("Found NDEF message with " + message.length +
+                (message.length === 1 ? " record" : " records"));
+            console.log(ndef.stringify(message));
+        }
+    });
+});
---
+++ index.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var fileName = 'ndef.bin';
+var ndef = require('ndef');
+
+function getUID(text) {
+    var regex = /^.*? with UID ([a-f0-9]+)/; //regex expression to extract UID from string like "Found Mifare Classic 1k with UID 564ecb50."
+    var result = text.match(regex);
+    if (result != undefined && result != null) {
+        return result[1];
+    }
+}
+
+function read(callback) {
+    var spawn = require('child_process').spawn;
+    var temp = require("temp").track();
+    var fileName = temp.path({
+        suffix: '.bin'
+    });
+    var command = spawn('mifare-classic-read-ndef', ['-y', '-o', fileName]);
+    var result = '';
+    var uid = '';
+    var errorMessage = '';
+
+    command.stdout.on('data', function(data) {
+        result += data.toString();
+    });
+
+    command.stderr.on('data', function(data) {
+        errorMessage += data.toString();
+    });
+
+    command.on('close', function(code) {
+        if (result.indexOf('Found') === -1) {
+            errorMessage = "No TAG found.";
+        }
+        uid = getUID(result);
+        if (code === 0 && errorMessage.length === 0) {
+            fs.readFile(fileName, function(err, data) {
+                callback(err, uid, data);
+            });
+        } else {
+            callback(errorMessage, uid);
+        }
+        fs.unlinkSync(fileName);
+    });
+}
+
+function format(callback) {
+    var errorMessage = '';
+    var spawn = require('child_process').spawn;
+    var command = spawn('mifare-classic-format', ['-y']);
+
+    command.stdout.on('data', function(data) {
+        process.stdout.write(data + "");
+    });
+    
+    command.stderr.on('data', function(data) {
+        errorMessage += data;
+    });
+
+    command.on('close', function(code) {
+        if (code === 0) {
+            callback(null);
+        } else {
+            callback(errorMessage);
+        }
+    });
+}
+
+module.exports = {
+    read: read,
+    format: format
+};
